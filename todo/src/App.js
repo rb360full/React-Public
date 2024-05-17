268174da5ca41af638444132d8d2f05de196ad71
Reza Bazdaran <rbazdaran@gmail.com>
2024-05-17 19:52:49 +0330

    "Removed constructor and state initialization from App component"

--- todo/src/App.js
+++ todo/src/App.js
@@ -3,12 +3,7 @@ import './App.css'
 import TodoListApp from "./Components/TodoListApp/TodoListApp";
 
 export default class App extends Component {
-  constructor (props){
-    super(props);
-   this.state = {
-    
-   }
-  }
+ 
   render() {
     return (
       <div className='App'>
