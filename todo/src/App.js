43c04dd383d992b650230d61c25dcbf51d462ffe
Reza Bazdaran <rbazdaran@gmail.com>
2024-05-17 10:37:36 +0330

    "Convert App component from functional to class component and add TODO APP heading"

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>TODO APP</h1>
+      </div>
+    )
+  }
 }
-
-export default App;
