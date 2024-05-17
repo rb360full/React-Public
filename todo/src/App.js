aaab608eccece9e91e9adb70de408e55364be032
Reza Bazdaran <rbazdaran@gmail.com>
2024-05-17 19:41:44 +0330

    #

--- todo/src/App.js
+++ todo/src/App.js
@@ -3,10 +3,18 @@ import './App.css'
 import TodoListApp from "./Components/TodoListApp/TodoListApp";
 
 export default class App extends Component {
+  constructor (props){
+    super(props);
+   this.state = {
+    
+   }
+  }
   render() {
     return (
       <div className='App'>
-        <TodoListApp></TodoListApp>
+        <TodoListApp>
+           
+        </TodoListApp>
       </div>
     )
   }
--- todo/src/Components/TodoListApp/TodoListApp.js
+++ todo/src/Components/TodoListApp/TodoListApp.js
@@ -4,6 +4,9 @@ import Header from '../Header/Header'
 import { FaPlusSquare } from "react-icons/fa";
 
 export default class TodoListApp extends Component {
+  constructor(props) {
+    
+  }
   render() {
     return (
       <div className='TodoListApp'>
@@ -13,6 +16,7 @@ export default class TodoListApp extends Component {
             <input className='todo-new' type='text' placeholder='Add a new todo' />
             <span className='plus-icon'><FaPlusSquare /></span>
           </div>
+          
         </div>
       </div>
     )
