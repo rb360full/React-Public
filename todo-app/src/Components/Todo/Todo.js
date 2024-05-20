5f5f720ca0fb8bcd35c4876533340c435818e271
Reza Bazdaran <rbazdaran@gmail.com>
2024-05-20 09:35:31 +0330

    "Added CSS styles for App, Todo, and TodoListApp components; implemented Todo component with icons and input field; added state and event handlers to TodoListApp component for submitting new todos and filtering todo list."

--- todo-app/src/Components/Todo/Todo.js
+++ todo-app/src/Components/Todo/Todo.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
 import './Todo.css'
+import { FaCheck } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 
 export default class Todo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: this.props.text,
+      isCompleted: false
+    }
+
+  }
   render() {
+
     return (
-      <div>Todo</div>
+      <div className='Todo'>
+        <div className='todo-container' >
+          <input className='todo-item' type='text' placeholder='' defaultValue={this.state.text} readOnly />
+          <div className='icons'>
+            <span className='icon check-icon'><FaCheck /></span>
+            <span className='icon del-icon'><MdDelete /></span>
+          </div>
+        </div>
+      </div>
     )
   }
 }
--- todo-app/src/Components/TodoListApp/TodoListApp.js
+++ todo-app/src/Components/TodoListApp/TodoListApp.js
@@ -6,26 +6,95 @@ import { FaPlusSquare } from "react-icons/fa";
 import { TbTriangleInvertedFilled } from "react-icons/tb";
 
 export default class TodoListApp extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      newTodo: {
+        id: 0,
+        text: '',
+        isCompleted: false
+      },
+      todoList: []
+
+    }
+  }
+
+  submitHandler = (event) => {
+    event.preventDefault()
+    const val = this.state.newTodo.text
+    const maxId = this.state.todoList.reduce(
+      (maxId, todo) => Math.max(maxId, todo.id),
+      0
+    );
+    const newTodoItem = {
+      id: maxId + 1,
+      text: val,
+      isCompleted: false,
+      status: 'All'
+    }
+    newTodoItem.text && this.setState({
+      todoList: [...this.state.todoList, newTodoItem],
+      newTodo: {
+        text: '',
+        isCompleted: false,
+
+      },
+
+
+    })
+
+
+  }
+  todoTextHandler = (event) => {
+    const val = event.target.value
+    this.setState({
+      newTodo: {
+        text: val,
+        isCompleted: false,
+        status: 'All'
+      }
+      , status: 'All'
+    })
+  }
+
+  filterHandler = (event) => {
+    const status = event.target.value
+    this.setState({
+      status: status
+    })
+    console.log(status);
+    // const filteredTodo = this.state.todoList.filter(item => item.status == status)
+    // console.log(filteredTodo);
+  }
+
 
   render() {
     return (
       <div className='TodoListApp'>
         <Header></Header>
-        <div className='todo-header'>
-          <div className='new-todo-container'>
-            <input className='todo-new' type='text' placeholder='Add a new todo' />
-            <span className='plus-icon'><FaPlusSquare /></span>
+        <form action="" onSubmit={(event) => this.submitHandler(event)}>
+          <div className='todo-header'>
+            <div className='new-todo-container'>
+              <input className='todo-new' type='text' onChange={this.todoTextHandler} placeholder='Add a new todo' value={this.state.newTodo.text} />
+              <span className='plus-icon'><button type='submit'><FaPlusSquare /></button></span>
+            </div>
+            <div className='filter-container'>
+              <select name="" id="" onChange={(event) => this.filterHandler(event)}>
+                <option value="All">All</option>
+                <option value="Completed">Completed</option>
+                <option value="Uncompleted">Uncompleted</option>
+              </select>
+              <span><TbTriangleInvertedFilled /></span>
+            </div>
           </div>
-          <div className='filter-container'>
-            <select name="" id="">
-              <option value="All">All</option>
-              <option value="Completed">Completed</option>
-              <option value="Uncompleted">Uncompleted</option>
-            </select>
-            <span><TbTriangleInvertedFilled /></span>
-          </div>
+        </form>
+        <div className='todo-list'>
+         
+          {this.state.todoList.length > 0 && this.state.todoList.map((todo) =>
+            <Todo key={todo.id} {...todo}></Todo>
+          ).filter(item => item.props.status == this.state.status)}
+
         </div>
-        <Todo></Todo>
       </div>
     )
   }
