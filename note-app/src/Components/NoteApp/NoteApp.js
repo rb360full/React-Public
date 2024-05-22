3927330b5bc79925811122cf23a2a3b42f046663
Reza Bazdaran <rbazdaran@gmail.com>
2024-05-22 11:03:53 +0330

    #

---
+++ note-app/src/Components/NoteApp/NoteApp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './NoteApp.css'
+
+export default class NoteApp extends Component {
+    render() {
+        return (
+            <div className='NoteApp'>
+                NoteApp
+            </div>
+        )
+    }
+}
