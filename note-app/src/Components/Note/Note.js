dc261f07a3d13b5b95db594dc290eb5f1fcea4a0
Reza Bazdaran <rbazdaran@gmail.com>
2024-05-22 18:44:01 +0330

    "Updated Note and NoteApp components with new styles, added functionality for adding and removing notes, and modified event handlers."

--- note-app/src/Components/Note/Note.js
+++ note-app/src/Components/Note/Note.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react'
 import './Note.css'
 
 export default class Note extends Component {
+  constructor(props) {
+    super(props)
+
+  }
   render() {
     return (
       <div className='Note'>
-        <div className='note-item' style={{backgroundColor:'red'}}>
-            <h3 className='note-text'>Text</h3>
-
+        <div className='note-item' style={{ backgroundColor: this.props.color }} onClick={() => this.props.onRemove(this.props)}>
+          <h3 className='note-text'>{this.props.text}</h3>
         </div>
       </div>
     )
--- note-app/src/Components/NoteApp/NoteApp.js
+++ note-app/src/Components/NoteApp/NoteApp.js
@@ -24,38 +24,62 @@ export default class NoteApp extends Component {
             ],
             notes: [],
             title: '',
-            inputColor : 'white'
+            inputColor: 'white'
         }
     }
 
-    changeColor = (color)=> {
-        
+    changeColor = (color) => {
+
         this.setState({
-            inputColor : color
+            inputColor: color
         })
     }
 
+    addNote = (text) => {
+        const maxId = this.state.notes.reduce(
+            (maxId, note) => Math.max(maxId, note.id),
+            0)
+        const newNote = {
+            id: maxId + 1,
+            text,
+            color: this.state.inputColor
+        }
+        const newNotesArray = [...this.state.notes, newNote]
+        console.log(newNotesArray);
+        this.state.title && this.setState({
+            notes: [...newNotesArray]
+        })
+        this.state.title = ''
+    }
+    submitHandler = (event, text) => {
+        event.preventDefault()
+        this.addNote(text)
+    }
 
     render() {
         return (
             <div className="NoteApp">
                 <h1 className="header">Welcome to Note App</h1>
-                <form action="">
+                <form action="" onSubmit={(event) => this.submitHandler(event, this.state.title)}>
                     <input type="text" value={this.state.title} onChange={(event) => this.setState({ title: event.target.value })}
-                     style={{backgroundColor:this.state.inputColor}}/>
+                        style={{ backgroundColor: this.state.inputColor }} />
                 </form>
                 <div className="color-box">
                     {this.state.colors.map((color) => {
-                        return <Color key={color} color={color} onColor={()=> this.changeColor(color)} />;
+                        return <Color key={color} color={color} onColor={() => this.changeColor(color)} />;
                     })}
                 </div>
                 <div className='btn-container'>
-                    <button className='plus-btn'><FaPlus /></button>
-                    <button className='remove-btn'><FaEraser /></button>
+                    <button className='plus-btn' onClick={() => this.addNote(this.state.title)}><FaPlus /></button>
+                    <button className='remove-btn' onClick={() => this.setState({ title: '' })}><FaEraser /></button>
                 </div>
 
                 <div className='note-list'>
-                    <Note></Note>
+
+                    {this.state.notes.map(item => {
+                        return <Note key={item.id} {...item} onRemove={() => this.setState({ notes: [...this.state.notes.filter(note => note !== item)] })}></Note>
+                    })}
+
                 </div>
             </div>
         );
