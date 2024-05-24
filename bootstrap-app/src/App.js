347f55d72221472c0b5491a0aeddfe6276c85a9b
Reza Bazdaran <rbazdaran@gmail.com>
2024-05-24 20:39:02 +0330

    "Updated App.css and App.js: added styles, removed Bootstrap components"

--- bootstrap-app/src/App.js
+++ bootstrap-app/src/App.js
@@ -1,28 +1,14 @@
 import React, { Component } from 'react'
 import './App.css'
-import { Button } from 'react-bootstrap'
-import { Alert } from 'react-bootstrap';
-import { Card } from 'react-bootstrap';
 
 
 
 export default class App extends Component {
-
-
+  
   render() {
     return (
       <div className='App'>
-
-        <Button variant="primary" onClick={() => console.log("Primary")}>
-          Primary
-        </Button>
-        <Alert variant="danger" dismissible onClose={this.handleCloseAlert}>
-          <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
-          <Alert.Link>Heading text</Alert.Link>
-        </Alert>
-        
-
-
+        <h1>App</h1>
       </div>
     )
   }
